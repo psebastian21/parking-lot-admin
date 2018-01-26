87ae6bae5a85176ffe7aca54e3d3c3cff5d8c12f
psebastian21 <psebastian21@gmail.com>
2018-01-26 16:44:15 -0500

    Directory tree structure created

---
+++ Java/parking-lot-paulo/src/main/java/co/com/ceiba/parkinglotpaulo/ParkingLotPauloApplication.java
@@ -0,0 +1,12 @@
+package co.com.ceiba.parkinglotpaulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotPauloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotPauloApplication.class, args);
+	}
+}
